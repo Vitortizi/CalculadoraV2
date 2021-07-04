fe36f158dc8d828e388bb4402b7af9c6691d873d
vitortizi <vitortizi@gmail.com>
2021-07-04 13:56:20 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  host: {
+    '(document:keydown)': 'getKey($event)'
+  }
 })
-export class AppComponent {
-  title = 'CalculadoraV2';
+export class AppComponent implements OnInit {
+
+  result: any;
+
+  ngOnInit(): void {
+    this.result = 0;
+  }
+
+  getKey(event: any) {
+    var validation = /[^0-9\,\.\*\+\-]+/g;
+    if (event.keyCode === 13) {
+      this.calculate();
+    }
+
+    if (validation.test(event.key)) {
+      event.preventDefault();
+    }
+  }
+
+  setValue(item: any) {
+    if (this.result == 0) {
+      this.result = "";
+    }
+
+    var number = this.result;
+    this.result = number + item;
+  }
+
+  clean() {
+    this.result = 0;
+  }
+
+  calculate() {
+    var resp = this.result;
+
+    if (resp) {
+      this.result = eval(resp);
+    } else {
+      this.result = 0;
+    }
+  }
 }
